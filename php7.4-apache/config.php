de4c3a04e5a994236e2357b795ba2d9a67a26dcf
Alex Chaus <chausme@gmail.com>
2023-03-01 13:57:09 +0400

    Remove config.php from PHP images and fix line endings

--- php7.4-apache/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('HOST_PROTOCOL', getenv('HOST_PROTOCOL'));
-define('HOST_ENV', getenv('HOST_ENV'));
-
-define('SERVERNAME', getenv('DB_SERVERNAME'));
-define('DBNAME', getenv('DB_NAME'));
-define('USERNAME', getenv('DB_USER_'));
-define('PASSWORD', getenv('DB_PASS'));
